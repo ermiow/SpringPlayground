d43d31cd67bb87da0c6f1f56b4dacfb0919d04ad
erminwang <erminwang1@gmail.com>
2025-07-04 19:57:25 -0700

    Spring Playground part 1 -

---
+++ src/main/java/com/loose/coupling/UserDatabaseProvider.java
@@ -0,0 +1,12 @@
+package com.loose.coupling;
+
+// A - MySQL, PostgreSQL
+// B - Web Service, MongoDB
+
+public class UserDatabaseProvider implements UserDataProvider {
+    @Override
+    public String getUserDetails() {
+        // Directly accessing DB
+        return "User Details From Database";
+    }
+}
